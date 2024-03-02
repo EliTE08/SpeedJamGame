d14e68f0676f09ffcfe3491f23cf6b224e1b4f56
Imated <lineborder122@gmail.com>
2024-03-01 20:48:55 -0500

    Bug Fixing, Some More Bug Fixing, And More Bug Fixing, And Swinging Works

--- SpeedJamGame/Assets/PlayerController.cs
+++ SpeedJamGame/Assets/PlayerController.cs
@@ -1,16 +1,20 @@
 using System;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : Singleton<PlayerController>
 {
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float accelTime;
+    [SerializeField] private float decelTime;
     [SerializeField] private float jumpForce;
     [SerializeField] private LayerMask groundLayer;
     
     [SerializeField] private GameObject targetSwingObject;
     [SerializeField] private float radius;
     [SerializeField] private float initialSwingSpeed = 5;
-    [SerializeField] private float decelFactor = 0.5f;
+    [SerializeField] private float swingSpeedGain = 5f;
+    [SerializeField] private float swingSpeedLoss = 2.5f;
+    [SerializeField] private float swingHeightDecel;
     [SerializeField] private LineRenderer swingLine;
 
     private Rigidbody2D _rb;
@@ -21,7 +25,8 @@ public class PlayerController : MonoBehaviour
     private bool _canSwing;
     private bool _isGrounded;
     private Vector3 _playerToAnchor;
-    private bool _isSwingingLeft;
+    private float _speed;
+    private bool _isSwinging;
 
     private void Start()
     {
@@ -29,25 +34,29 @@ public class PlayerController : MonoBehaviour
         _canSwing = true;
         _isGrounded = true;
         _target = targetSwingObject.transform.position;
+        _speed = moveSpeed;
     }
 
     private void Update()
     {
         _isGrounded = _rb.IsTouchingLayers(groundLayer);
         _canSwing = InRange(_target);
+        _rb.gravityScale = _isSwinging ? 0f : 1f;
 
         MovePlayer();
-        if (Input.GetButtonDown("Jump") && _canSwing)
-            StartSwing();
         if (Input.GetButton("Jump") && _canSwing)
-            Swing();
-        else
-            StopSwing();
-    }
+        {
+            if (!_isSwinging)
+            {
+                if(transform.position.x <= targetSwingObject.transform.position.x)
+                    Swing();
+            }
+            else
+                Swing();
+        }
 
-    private void StartSwing()
-    {
-        _isSwingingLeft = _rb.position.x > targetSwingObject.transform.position.x;
+        if(Input.GetButtonUp("Jump"))
+            StopSwing();
     }
 
     private void Jump()
@@ -58,6 +67,8 @@ public class PlayerController : MonoBehaviour
     
     private void MovePlayer()
     {
+        if(_isSwinging)
+            return;
         _horiz = Input.GetAxis("Horizontal");
         _vert = Input.GetAxis("Vertical"); 
 
@@ -66,32 +77,45 @@ public class PlayerController : MonoBehaviour
             Jump();
         if(_horiz != 0)
             transform.localScale = new Vector3(_horiz > 0 ? 1 : -1, 1, 1);
-        
-        _rb.velocity += new Vector2(_horiz * moveSpeed, 0f);
-        _rb.velocity = new Vector2(Mathf.Clamp(_rb.velocity.x, -moveSpeed, moveSpeed), _rb.velocity.y);
+        if(_horiz != 0)
+            Accelerate(_horiz, moveSpeed);
+        if(_horiz == 0 && !_isSwinging && _isGrounded)
+            Decelerate();
+    }
+    
+    private void Accelerate(float direction, float maxSpeed)
+    {
+        var acceleration = (direction * maxSpeed - _rb.velocity.x) / accelTime;
+        _rb.velocity += new Vector2(acceleration * Time.deltaTime * 100, 0f);
+    }
+
+    private void Decelerate()
+    {
+        var deceleration = (0 - _rb.velocity.x) / decelTime;
+        _rb.velocity += new Vector2(deceleration * Time.deltaTime * 100, 0f);
     }
 
     private void Swing()
     {
-        Vector2 toAnchor = (Vector2)targetSwingObject.transform.position - _rb.position;
-        Vector2 onRadius = toAnchor.normalized * radius;
-        Vector2 tangent = new Vector2(-onRadius.y, onRadius.x).normalized;
-        
-        // Dynamically adjust swing direction based on player's position relative to the swing point
-        if (_isSwingingLeft)
+        _isSwinging = true;
+        var toAnchor = (Vector2)targetSwingObject.transform.position - _rb.position;
+        var onRadius = toAnchor.normalized * radius;
+        var tangent = new Vector2(onRadius.y, -onRadius.x).normalized;
+        _rb.velocity = tangent * initialSwingSpeed;
+        if (transform.position.x >= targetSwingObject.transform.position.x)
+            swingSpeedLoss = Mathf.Abs(swingSpeedLoss);
+        else 
+            swingSpeedLoss = -Mathf.Abs(swingSpeedLoss);
+        if (transform.position.y <= swingHeightDecel)
         {
-            // Player is to the right of the swing point, swing counter-clockwise
-            tangent = new Vector2(-onRadius.y, onRadius.x).normalized;
+            if (transform.position.x >= targetSwingObject.transform.position.x)
+                initialSwingSpeed += swingSpeedGain / 100 * Mathf.Abs(_speed);
+            else
+                initialSwingSpeed -= swingSpeedLoss / 100 * Mathf.Abs(_speed);
         }
         else
-        {
-            // Player is to the left of the swing point, swing clockwise
-            tangent = new Vector2(onRadius.y, -onRadius.x).normalized;
-        }
+            initialSwingSpeed -= swingSpeedLoss / 100 * Mathf.Abs(_speed);
 
-        // Apply the tangential velocity to swing, optionally maintain any existing vertical motion for realism
-        _rb.velocity = tangent * initialSwingSpeed;
-        
         swingLine.enabled = true;
         swingLine.SetPosition(0, transform.position);
         swingLine.SetPosition(1, targetSwingObject.transform.position);
@@ -99,12 +123,17 @@ public class PlayerController : MonoBehaviour
     
     private void StopSwing()
     {
+        _isSwinging = false;
         swingLine.enabled = false;
     }
-
-
+    
     private bool InRange(Vector2 target)
     {
         return radius >= Vector2.Distance(transform.position, target);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(new Vector3(-999f, targetSwingObject.transform.position.y), new Vector3(999f, swingHeightDecel));
     }
 }
