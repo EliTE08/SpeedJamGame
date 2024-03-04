743e4f1659075089268f5ec5fa50263af3742b21
JoshThePlug <josh21pcs@gmail.com>
2024-03-03 22:24:14 -0500

    Timer

--- SpeedJamGame/Assets/TimerScript.cs
+++ SpeedJamGame/Assets/TimerScript.cs
@@ -9,17 +9,15 @@ public class TimerScript : MonoBehaviour
     public TMP_Text timerText;
     private float startTime;
     private bool timerActive;
-    
+
     void Start()
     {
         timerActive = false;
     }
 
-    
     void Update()
     {
-
-        if(!timerActive && Input.anyKey)
+        if (!timerActive && Input.anyKeyDown)
         {
             StartTimerWithInput();
         }
@@ -28,10 +26,11 @@ public class TimerScript : MonoBehaviour
         {
             float t = Time.time - startTime;
 
-            string minutes = ((int)t / 60).ToString("00");
-            string seconds = (t % 60).ToString("00");
+            string minutes = Mathf.Floor(t / 60).ToString("00");
+            string seconds = Mathf.Floor(t % 60).ToString("00");
+            string milliseconds = Mathf.Floor((t * 1000) % 1000).ToString("000");
 
-            timerText.text = minutes + ":" + seconds;
+            timerText.text = minutes + ":" + seconds + ":" + milliseconds;
         }
     }
 
@@ -48,5 +47,4 @@ public class TimerScript : MonoBehaviour
     {
         timerActive = false;
     }
-
 }
